df910551b9cb094506ed44bd6e7a8e0ff668a2b8
Daniel Shapiro <d.shapiro@berkeley.edu>
2018-02-24 00:05:16 -0800

    delay missile prefab

---
+++ Alakajam/Assets/DelayMissile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayMissile : MonoBehaviour {
+
+    private float launchDelay = 1f;
+    private float accelTime = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
