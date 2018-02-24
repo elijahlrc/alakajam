0933eb72cd0e021ab8b17289e3a226c99965b767
Elijah <elijahlrc@berkeley.edu>
2018-02-23 23:36:52 -0800

    comming player component class

---
+++ Alakajam/Assets/Scripts/PlayerComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(Network.isServer)
+        {
+            bool thrusting = Input.GetKey("mouse 0");
+            if (thrusting)
+            {
+                Vector2 goalPos = Input.mousePosition;
+                print(goalPos);
+            }
+        }
+    }
+
+
+}
